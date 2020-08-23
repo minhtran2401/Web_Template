9bcf5e1cb5643ffefd367ce28b424e9f88223274
shin254 <tiennvps10480@fpt.edu.vn>
2020-08-23 18:25:26 +0700

    menu, header, footer, home, index

---
+++ resources/views/header.blade.php
@@ -0,0 +1,47 @@
+<!-- Navigation Bar-->
+<div class="tagline hidden-md">
+    <div class="container">
+        <div class="float-left">
+            <div class="phone">
+                <i class="fas fa-phone"></i> +1 123 456 78 90
+            </div>
+        </div>
+        <div class="float-right">
+            <ul class="top_socials">
+                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+            </ul>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+
+<!-- Navigation Bar-->
+@yield('menu')
+<!-- End Navigation Bar-->
+
+<!-- HOME START-->
+<section class="bg-home" style="background-image:url('assets/images/home/bg-home.jpg');">
+    <div class="bg-overlay"></div>
+    <div class="home-center">
+        <div class="home-desc-center">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <div class="title-heading text-center">
+                            <h1 class="">Financial Planners</h1>
+                            <p class="text-white-50 mx-auto">On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.</p>
+                            <div class="mt-3">
+                                <a href="#" class="btn btn-custom mr-3">Contact Us</a>
+                                <a href="#" class="btn btn-outline-white">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/menu.blade.php
@@ -0,0 +1,101 @@
+<header id="topnav" class="defaultscroll fixed-top navbar-sticky sticky">
+    <div class="container">
+        <!-- Logo container-->
+        <div>
+            <a href="index-2.html" class="logo">
+                <img src="assets/images/logo.png" alt="missing_logo" height="20">
+            </a>
+        </div>
+        <!-- End Logo container-->
+        <div class="menu-extras">
+            <div class="menu-item">
+                <!-- Mobile menu toggle-->
+                <a class="navbar-toggle">
+                    <div class="lines">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </a>
+                <!-- End mobile menu toggle-->
+            </div>
+        </div>
+
+        <div id="navigation">
+            <!-- Navigation Menu-->
+            <ul class="navigation-menu">
+                <li class="has-submenu">
+                    <a href="#">Home</a><span class="menu"></span>
+                </li>
+
+                <li class="has-submenu">
+                    <a href="javascript:void(0)">About</a><span class="menu-arrow"></span>
+                    <ul class="submenu">
+                        <li><a href="about.html">About</a></li>
+                        <li><a href="history.html">History</a></li>
+                        <li class="has-submenu"><a href="javascript:void(0)">Services</a><span class="submenu-arrow"></span>
+                            <ul class="submenu">
+                                <li><a href="service.html">Services</a></li>
+                                <li><a href="service-image.html">Service With Image</a></li>
+                                <li><a href="service-detail.html">Single Service</a></li>
+                            </ul>
+                        </li>  
+                        <li class="has-submenu"><a href="javascript:void(0)">Team Members</a><span class="submenu-arrow"></span>
+                            <ul class="submenu">
+                                <li><a href="team-grid.html">Grid Teams</a></li>
+                                <li><a href="team-list.html">List Teams</a></li>
+                                <li><a href="team-detail.html">Single Team</a></li>
+                            </ul>
+                        </li>
+                        <li class="has-submenu"><a href="javascript:void(0)">Work</a><span class="submenu-arrow"></span>
+                            <ul class="submenu">
+                                <li><a href="work-col-three.html">Three Column Portfolio</a></li>                                 
+                                <li><a href="work-col-four.html">Four Column Portfolio</a></li>                                 
+                                <li><a href="work-col-six.html">Six Column Portfolio</a></li>                                 
+                                <li><a href="work-single.html">Single Portfolio</a></li>                                
+                            </ul>
+                        </li>
+                    </ul> 
+                </li>
+
+                <li class="has-submenu">
+                    <a href="javascript:void(0)">Thể Loại</a>
+                    <span class="menu-arrow"></span>
+                    <ul class="submenu megamenu">
+                        <li>
+                            <ul>
+                                <li><a href="faq.html"><b class="mdi mdi-chevron-right"></b> FAQ's</a></li>
+                                
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="has-submenu">
+                    <a href="javascript:void(0)">Themes Wordpress</a>
+                </li>
+
+                <li class="has-submenu">
+                    <a href="contact">Liên Hệ</a><span class="menu"></span>
+                </li>
+
+                <li class="dropdown d-none d-md-inline-block">
+                    <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#" role="button"
+                        aria-haspopup="false" aria-expanded="false">
+                        <i class="fas fa-search noti-icon"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-search">
+                        <div>
+                            <form role="search" class="navbar-form">
+                                <input type="text" placeholder="Search..." name="s" class="form-control search-bar">
+                                <a href="#" class="btn-search"><i class="fa fa-search"></i></a>
+                            </form>
+                        </div>
+
+                    </div>
+                </li>
+            </ul>
+            <!-- End navigation menu-->
+        </div>
+    </div>
+</header>
