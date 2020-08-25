3e7337d5b59ff057c4a34e6ddcc9af3eddb1a95f
minh2401 <minhtnps10422@fpt.edu.vn>
2020-08-25 14:58:10 +0700

    fix some bug

--- template.sql
+++ template.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th8 24, 2020 lúc 06:18 PM
--- Phiên bản máy phục vụ: 10.1.40-MariaDB
--- Phiên bản PHP: 7.3.5
+-- Thời gian đã tạo: Th8 25, 2020 lúc 09:57 AM
+-- Phiên bản máy phục vụ: 10.4.14-MariaDB
+-- Phiên bản PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `template`
+-- Cơ sở dữ liệu: `laravel`
 --
 
 -- --------------------------------------------------------
@@ -52,7 +51,24 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2020_08_24_160336_sua_tabletemplate', 1);
+(1, '2020_08_24_160336_sua_tabletemplate', 1),
+(2, '2020_08_25_074104_sua_tabletypletemp', 2),
+(3, '2020_08_25_074922_tao_tabletags', 3),
+(4, '2020_08_25_075224_sua_tabletags', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tags`
+--
+
+CREATE TABLE `tags` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(225) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Anhien` int(2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -68,9 +84,18 @@ CREATE TABLE `template` (
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `id_type` int(1) NOT NULL,
   `tags` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Anhien` int(2) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `template`
+--
+
+INSERT INTO `template` (`id_temp`, `name`, `path`, `size`, `description`, `id_type`, `tags`, `Anhien`, `created_at`, `updated_at`) VALUES
+(1, 'FPT polytechnic', 'upload/fptpolytecnic', '15MB', 'Test thôi', 1, 'giáo dục, trường học', 1, NULL, NULL),
+(2, 'Test', 'upload/test', '2Mb', 'test', 2, 'doanh nghiệp, cơ quan', 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -80,8 +105,19 @@ CREATE TABLE `template` (
 
 CREATE TABLE `template_type` (
   `id_type` int(1) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `Anhien` int(2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `template_type`
+--
+
+INSERT INTO `template_type` (`id_type`, `name`, `Anhien`, `created_at`, `updated_at`) VALUES
+(1, 'Giáo Dục', 1, NULL, NULL),
+(2, 'Doanh Nghiệp', 1, NULL, NULL);
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -97,6 +133,12 @@ ALTER TABLE `contact`
 -- Chỉ mục cho bảng `migrations`
 --
 ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `tags`
+--
+ALTER TABLE `tags`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -126,19 +168,35 @@ ALTER TABLE `contact`
 -- AUTO_INCREMENT cho bảng `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT cho bảng `template`
 --
 ALTER TABLE `template`
-  MODIFY `id_temp` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_temp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT cho bảng `template_type`
 --
 ALTER TABLE `template_type`
-  MODIFY `id_type` int(1) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_type` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `template`
+--
+ALTER TABLE `template`
+  ADD CONSTRAINT `template_ibfk_1` FOREIGN KEY (`id_type`) REFERENCES `template_type` (`id_type`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
