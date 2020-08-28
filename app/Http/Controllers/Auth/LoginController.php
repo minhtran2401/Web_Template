4c554898282518b7a468351d877ea9231b533a0c
Hugo123N <nhatntps10403@fpt.edu.vn>
2020-08-28 21:31:12 +0700

    ok

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/admin';
+    protected $redirectTo = RouteServiceProvider::HOME;
+
     /**
      * Create a new controller instance.
      *
@@ -35,6 +36,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,7 @@ Route::get('/admin', function(){
     return view('admin/dashboard');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
